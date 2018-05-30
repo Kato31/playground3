abde40f5bde96b7352bc8e6c8266c3c911e1f417
Kato31 <blzrd3@yandex.ru>
2018-05-31 01:44:07 +0300

    mess

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
-
 public class Event {
+    long node;
+    long key;
+    long offset;
+    String target;
+    Object payload;
+
+    public long getNode() {
+        return node;
+    }
+
+    public void setNode(long node) {
+        this.node = node;
+    }
+
+    public long getKey() {
+        return key;
+    }
+
+    public void setKey(long key) {
+        this.key = key;
+    }
+
+    public long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(long offset) {
+        this.offset = offset;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "node=" + node +
+                ", key=" + key +
+                ", offset=" + offset +
+                ", target='" + target + '\'' +
+                ", payload=" + payload +
+                '}';
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    public static void main(String[] args) {
+        Event event = new Event();
+        event.setKey(100L);
+        event.setNode(101L);
+        event.setOffset(102L);
+        event.setTarget("target1");
+        String payload = "payload";
+        event.setPayload(payload);
+
+        LoggerFactory.getLogger(Event.class).error(event.toString());
+        LoggerFactory.getLogger(Event.class).debug(event.toString());
+        LoggerFactory.getLogger(Event.class).info(event.toString());
+        LoggerFactory.getLogger(Event.class).trace(event.toString());
+
+    }
 }
